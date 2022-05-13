6000b414fb6a51404c9c59be2e676dcd93cefe48
Danish Ali <88722224+malikdanishalii@users.noreply.github.com>
2022-05-13 22:29:23 +0500

    app logo work

--- lib/Screens/aboutus_screen.dart
+++ lib/Screens/aboutus_screen.dart
@@ -1,28 +1,55 @@
 // ignore_for_file: prefer_const_constructors
 
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:remove_h2o/Screens/home/Home_screen.dart';
 import 'package:remove_h2o/aboutDetail.dart';
 import 'package:remove_h2o/navigartion_drawer.dart';
 import 'package:remove_h2o/size_config.dart';
+import 'package:flutter/material.dart';
 
 class abtpage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        iconTheme: IconThemeData(
-          color: Colors.blue,
-        ),
-        toolbarHeight: 90,
-        backgroundColor: Colors.white,
-        brightness: Brightness.light,
-        centerTitle: true,
-        title: Image.asset(
-          "assets/images/logo.png",
-          height: getProportionateScreenHeight(270),
-        ),
-      ),
+          iconTheme: IconThemeData(
+            color: Colors.blue,
+          ),
+          toolbarHeight: 90,
+          backgroundColor: Colors.white,
+          brightness: Brightness.light,
+          centerTitle: true,
+          title: StreamBuilder<QuerySnapshot>(
+              stream: FirebaseFirestore.instance
+                  .collection('Vendor Data')
+                  .snapshots(),
+              builder: (BuildContext context,
+                  AsyncSnapshot<QuerySnapshot> snapshot) {
+                if (snapshot.hasError) {
+                  return Text('Something went wrong');
+                }
+
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return Text("Loading");
+                }
+
+                return ListView(
+                    shrinkWrap: true,
+                    // itemCount: snapshot.data!.docs.length,
+                    // itemBuilder: ((context, index) {
+                    children:
+                        snapshot.data!.docs.map((DocumentSnapshot document) {
+                      Map<String, dynamic> data =
+                          document.data()! as Map<String, dynamic>;
+
+                      return Image.asset(data['logo']);
+                    }).toList());
+                //     return Text('nodata');
+                //   }),
+                // );
+              })),
       drawer: NavigationDrawer(),
       body: SingleChildScrollView(
         padding: EdgeInsets.symmetric(vertical: 20, horizontal: 20),
@@ -54,30 +81,61 @@ class abtpage extends StatelessWidget {
                   fontWeight: FontWeight.bold,
                 ),
               ),
-              Container(
-                height: 200.0,
-                width: 320.0,
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                    image: AssetImage('assets/images/ambulance.jpg'),
-                    fit: BoxFit.fill,
-                  ),
-                  shape: BoxShape.rectangle,
-                ),
-              ),
-              RichText(
-                text: TextSpan(
-                  style: TextStyle(color: Colors.black, fontSize: 16),
-                  // ignore: prefer_const_literals_to_create_immutables
-                  children: <TextSpan>[
-                    //TextSpan(text: '', style: TextStyle(color: Colors.blue)),
-                    TextSpan(
-                        text:
-           'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typeset'),
-                    //TextSpan(text: 'com', style: TextStyle(decoration: TextDecoration.underline))
-                  ],
-                ),
-              ),
+              StreamBuilder<QuerySnapshot>(
+                  stream: FirebaseFirestore.instance
+                      .collection('Vendor Data')
+                      .snapshots(),
+                  builder: (BuildContext context,
+                      AsyncSnapshot<QuerySnapshot> snapshot) {
+                    if (snapshot.hasError) {
+                      return Text('Something went wrong');
+                    }
+
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return Text("Loading");
+                    }
+
+                    return ListView(
+                        shrinkWrap: true,
+                        // itemCount: snapshot.data!.docs.length,
+                        // itemBuilder: ((context, index) {
+                        children: snapshot.data!.docs
+                            .map((DocumentSnapshot document) {
+                          Map<String, dynamic> data =
+                              document.data()! as Map<String, dynamic>;
+
+                          return Column(
+                            children: [
+                              Container(
+                                height: 200.0,
+                                width: 320.0,
+                                decoration: BoxDecoration(
+                                  image: DecorationImage(
+                                    image: AssetImage(data['imageUrl']),
+                                    fit: BoxFit.fill,
+                                  ),
+                                  shape: BoxShape.rectangle,
+                                ),
+                              ),
+                              RichText(
+                                text: TextSpan(
+                                  style: TextStyle(
+                                      color: Colors.black, fontSize: 16),
+                                  // ignore: prefer_const_literals_to_create_immutables
+                                  children: <TextSpan>[
+                                    //TextSpan(text: '', style: TextStyle(color: Colors.blue)),
+                                    TextSpan(text: data['workadress']),
+                                    //TextSpan(text: 'com', style: TextStyle(decoration: TextDecoration.underline))
+                                  ],
+                                ),
+                              ),
+                            ],
+                          );
+                        }).toList());
+                    //     return Text('nodata');
+                    //   }),
+                    // );
+                  })
             ],
           ),
         ),
--- lib/aboutDetail.dart
+++ lib/aboutDetail.dart
@@ -2,6 +2,7 @@
 
 import 'dart:io';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
@@ -19,11 +20,13 @@ class AboutDetail extends StatefulWidget {
 
 class _AboutDetailState extends State<AboutDetail> {
   final _formKey = GlobalKey<FormState>();
+  final _auth = FirebaseAuth.instance;
   firebase_storage.Reference imageRef = firebase_storage
       .FirebaseStorage.instance
       .ref('Images / ${DateTime.now()}');
   TextEditingController workadresscontroller = TextEditingController();
   File? _image;
+  File? _logo;
   Future getImage() async {
     final image = await ImagePicker().pickImage(source: ImageSource.camera);
     if (image != null) {
@@ -33,48 +36,79 @@ class _AboutDetailState extends State<AboutDetail> {
       });
     }
   }
-void ShowSnackBar(BuildContext context) {
+
+  Future getlogo() async {
+    final image = await ImagePicker().pickImage(source: ImageSource.camera);
+    if (image != null) {
+      final imageTem = File(image.path);
+      setState(() {
+        _logo = imageTem;
+      });
+    }
+  }
+
+  void ShowSnackBar(BuildContext context) {
     final snackBar = SnackBar(
       content: Text('Data uploaded successfully'),
       backgroundColor: Colors.blue,
     );
     ScaffoldMessenger.of(context).showSnackBar(snackBar);
   }
+
   Future submitData() async {
     final isValid = _formKey.currentState!.validate();
 
     if (isValid) {
       _formKey.currentState!.save();
-     
+
       workadresscontroller.text.trim();
-    UploadTask uploadTask = imageRef.putFile(_image!);
-    await Future.value(uploadTask);
-    var imageUrl = await imageRef.getDownloadURL();
-      FirebaseFirestore.instance.collection('Vendor Data').add({
+      UploadTask uploadTask = imageRef.putFile(_image!);
+      await Future.value(uploadTask);
+      var imageUrl = await imageRef.getDownloadURL();
+      UploadTask uploadTask1 = imageRef.putFile(_logo!);
+      await Future.value(uploadTask);
+      var imageUrl1 = await imageRef.getDownloadURL();
+      FirebaseFirestore.instance
+          .collection('Vendor Data')
+          .doc(_auth.currentUser!.uid)
+          .set({
         'workadress': workadresscontroller.text.trim(),
-        'imageUrl':imageUrl,
+        'imageUrl': imageUrl,
+        'logo': imageUrl1,
       }).then((value) => ShowSnackBar(context));
     }
   }
 
-
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        iconTheme: IconThemeData(
-          color: Colors.blue,
-        ),
-        toolbarHeight: 90,
-        backgroundColor: Colors.white,
-        brightness: Brightness.light,
-        centerTitle: true,
-        title: Image.asset(
-          "assets/images/logo.png",
-          height: getProportionateScreenHeight(270),
-        ),
-      ),
+      appBar: _logo == null
+          ? AppBar(
+              iconTheme: IconThemeData(
+                color: Colors.blue,
+              ),
+              toolbarHeight: 90,
+              backgroundColor: Colors.white,
+              brightness: Brightness.light,
+              centerTitle: true,
+              title: TextButton(
+                onPressed: getlogo,
+                child: Text('Update Logo'),
+              ),
+            )
+          : AppBar(
+              iconTheme: IconThemeData(
+                color: Colors.blue,
+              ),
+              toolbarHeight: 90,
+              backgroundColor: Colors.white,
+              brightness: Brightness.light,
+              centerTitle: true,
+              title: Image.file(
+                _logo!,
+                height: getProportionateScreenHeight(270),
+              ),
+            ),
       drawer: NavigationDrawer(),
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
@@ -153,7 +187,7 @@ void ShowSnackBar(BuildContext context) {
     return TextFormField(
       validator: (value) {
         if (value!.isEmpty) {
-    return 'Please Enter Description.';
+          return 'Please Enter Description.';
         }
         return null;
       },
